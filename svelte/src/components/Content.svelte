3424efd3b6bfc67fddd1d3c481bebb54464665c5
MrAnyx <1ptitigre@gmail.com>
2020-03-06 09:56:35 +0100

    [fix] bug routing get params from url svelte & finish svelte project

--- svelte/src/components/Content.svelte
+++ svelte/src/components/Content.svelte
@@ -1,10 +1,22 @@
 <script>
-import { Route } from "svelte-routing"
+
+import {afterUpdate} from "svelte";
+import data from "../assets/data.js"
+
+export let page;
+let content = ""
+
+afterUpdate(() => {
+   content = data[page]
+});
+
+
+
 
 </script>
 
 <main>
-   <p id="page">Eu enim elit tempor ut consectetur enim officia consectetur sit. Ad dolore laboris irure qui labore id magna tempor velit adipisicing qui. Cillum culpa officia reprehenderit nostrud excepteur magna reprehenderit enim dolor non. Aliquip velit consectetur ullamco culpa laboris cupidatat ut proident quis dolore adipisicing.Id duis officia ullamco do excepteur qui fugiat irure commodo elit cillum dolore adipisicing. Consectetur duis aute cillum labore ullamco velit amet ea nostrud pariatur est aliqua proident ex. Cillum ex occaecat qui tempor aute veniam.Sit esse eiusmod laborum culpa aliquip. Eu est commodo aute eiusmod qui pariatur aliquip dolor anim veniam id nulla. Adipisicing nisi laborum laboris est excepteur culpa cupidatat. Aliqua anim aliqua id proident culpa laboris qui tempor elit minim dolor anim sit laborum.Fugiat nisi qui officia aute cupidatat laborum velit do anim ipsum incididunt eiusmod commodo. Reprehenderit consequat cupidatat pariatur ad. Quis ad minim ex quis elit anim Lorem ipsum. Pariatur non magna minim mollit aliqua cillum dolor culpa. Dolor ea adipisicing deserunt ea irure eiusmod duis ex mollit tempor. Occaecat quis do nostrud veniam.</p>
+   <p id="page">{content}</p>
 </main>
 
 <style>
