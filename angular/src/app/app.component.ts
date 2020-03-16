56959fd56e9e7eb5e53430032b3389db5dcf729d
MrAnyx <1ptitigre@gmail.com>
2020-03-16 10:35:25 +0100

    [add] new angular

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Angular app</h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'angular';
+}
