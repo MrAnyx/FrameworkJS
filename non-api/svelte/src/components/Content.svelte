1c5b5130eafb1a7311ba528f5ecdbc80a4f7ca7e
MrAnyx <1ptitigre@gmail.com>
2020-03-20 11:07:02 +0100

    [fix] structure folder

---
+++ non-api/svelte/src/components/Content.svelte
@@ -0,0 +1,28 @@
+<script>
+
+   import { afterUpdate } from "svelte";
+   import data from "../assets/data.js"
+
+   export let page;
+
+   let content = ""
+
+   afterUpdate(() => {
+      content = data[page]
+   });
+
+</script>
+
+<main>
+   <p id="page">{content}</p>
+</main>
+
+<style>
+   #page{
+      
+      width: 80%;
+      color: #2e2e2e;
+      text-align: justify;
+      margin: 50px auto 0 auto;
+   }
+</style>
